526217f90d7481271f0b8d2f3b0b45f88b6441d3
jaydeepkhatri <jaydeepkhatri79@gmail.com>
2021-07-12 15:48:09 +0530

    Added Social media links

--- src/components/TiltCard.js
+++ src/components/TiltCard.js
@@ -16,16 +16,16 @@ const TiltCard = () => {
                 <button className="contact">Contact Me</button>
                 <ul className="social">
                     <li className="list-item facebook" title="Facebook">
-                        <a href="#" className="link"><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="https://www.facebook.com/jaydeepkhatri79/" className="link"><FontAwesomeIcon icon={faFacebook} /></a>
                     </li>
                     <li className="list-item twitter" title="Twitter">
-                        <a href="" className="link"><FontAwesomeIcon icon={faTwitter} /></a>
+                        <a href="https://twitter.com/jaydeepkhatri79" className="link"><FontAwesomeIcon icon={faTwitter} /></a>
                     </li>
                     <li className="list-item instagram" title="Instagram">
-                        <a href="#" className="link"><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a href="https://www.instagram.com/jaydeepkhatri79/" className="link"><FontAwesomeIcon icon={faInstagram} /></a>
                     </li>
                     <li className="list-item linkedin" title="LinkedIn">
-                        <a href="#" className="link"><FontAwesomeIcon icon={faLinkedin} /></a>
+                        <a href="https://www.linkedin.com/in/jaydeep-khatri/" className="link"><FontAwesomeIcon icon={faLinkedin} /></a>
                     </li>
                 </ul>
             </Tilt>
